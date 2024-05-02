7d7b85ac6e45bebdfa67aa80b011466c7394c383
Ezzalibrahim <brahim.ezzali@hotmail.com>
2024-05-02 12:15:50 +0100

    create main dashboard page component

---
+++ src/app/components/admin-dashboard/pages/main-dashboard/main-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-dashboard',
+  templateUrl: './main-dashboard.component.html',
+  styleUrls: ['./main-dashboard.component.css']
+})
+export class MainDashboardComponent {
+
+}
