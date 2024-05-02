c0c09a0ef41a896b3d72542d935494cc7ed2626f
Ezzalibrahim <brahim.ezzali@hotmail.com>
2024-05-02 12:13:39 +0100

    create dashboard header component

---
+++ src/app/components/admin-dashboard/header-dashboard/header-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-dashboard',
+  templateUrl: './header-dashboard.component.html',
+  styleUrls: ['./header-dashboard.component.css']
+})
+export class HeaderDashboardComponent {
+
+}
