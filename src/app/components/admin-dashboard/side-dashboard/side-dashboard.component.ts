c900651544512d99842845282fb15934ecb2dcef
Ezzalibrahim <brahim.ezzali@hotmail.com>
2024-05-02 12:16:19 +0100

    create side bar component

---
+++ src/app/components/admin-dashboard/side-dashboard/side-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-dashboard',
+  templateUrl: './side-dashboard.component.html',
+  styleUrls: ['./side-dashboard.component.css']
+})
+export class SideDashboardComponent {
+
+}
