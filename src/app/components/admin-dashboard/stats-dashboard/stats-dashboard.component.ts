b1b041ee3ac855749b1fc2c0d2e932396aa1ea91
Ezzalibrahim <brahim.ezzali@hotmail.com>
2024-05-02 12:19:08 +0100

    create statstics component

---
+++ src/app/components/admin-dashboard/stats-dashboard/stats-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-dashboard',
+  templateUrl: './stats-dashboard.component.html',
+  styleUrls: ['./stats-dashboard.component.css']
+})
+export class StatsDashboardComponent {
+
+}
